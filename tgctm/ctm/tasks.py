766d0490dffa7b26bb0e03e8f966bc325f34bf00
Glenn <glenn@kandu.no>
2023-04-08 13:00:28 +0000

    fix: try to send slack notifications if possible
    Signed-off-by: Glenn <glenn@kandu.no>

--- tgctm/ctm/tasks.py
+++ tgctm/ctm/tasks.py
@@ -9,21 +9,24 @@ timeloop = Timeloop()
 
 @timeloop.job(interval=timedelta(minutes=1))
 def send_reminders():
-    timeslotusers = TaskTimeSlotUser.objects.all()
-    for timeslotuser in timeslotusers:
-        # if 5 minutes left, send reminder
-        if timeslotuser.reminder_status != "TW" and timeslotuser.timeslot.starts < timezone.now() + timedelta(minutes=10) and timeslotuser.timeslot.starts > timezone.now():
-            print("Send reminder to " + timeslotuser.user.username + " for task " + str(timeslotuser.timeslot) + " (10 minutes)")
-            success = send_message_to_user(timeslotuser.user, "🔔 Ti minutter igjen til `" + str(timeslotuser.timeslot) + "`! Oppmøte er ved trappen utenfor logistikk.")
-            if success:
-                timeslotuser.reminder_status = "TW"
-                timeslotuser.save()
-        # if 60 minutes left, send reminder
-        elif timeslotuser.reminder_status == "NO" and timeslotuser.timeslot.starts < timezone.now() + timedelta(hours=2) and timeslotuser.timeslot.starts > timezone.now():
-            print("Send reminder to " + timeslotuser.user.username + " for task " + str(timeslotuser.timeslot) + " (2 hours left)")
-            success = send_message_to_user(timeslotuser.user, "🔔 Hei, oppgaven `" + str(timeslotuser.timeslot) + "` starter om under to timer. Oppmøte er ved trappen utenfor logistikk.")
-            if success:
-                timeslotuser.reminder_status = "ON"
-                timeslotuser.save()
+    try:
+        timeslotusers = TaskTimeSlotUser.objects.all()
+        for timeslotuser in timeslotusers:
+            # if 5 minutes left, send reminder
+            if timeslotuser.reminder_status != "TW" and timeslotuser.timeslot.starts < timezone.now() + timedelta(minutes=10) and timeslotuser.timeslot.starts > timezone.now():
+                print("Send reminder to " + timeslotuser.user.username + " for task " + str(timeslotuser.timeslot) + " (10 minutes)")
+                success = send_message_to_user(timeslotuser.user, "🔔 Ti minutter igjen til `" + str(timeslotuser.timeslot) + "`! Oppmøte er ved trappen utenfor logistikk.")
+                if success:
+                    timeslotuser.reminder_status = "TW"
+                    timeslotuser.save()
+            # if 60 minutes left, send reminder
+            elif timeslotuser.reminder_status == "NO" and timeslotuser.timeslot.starts < timezone.now() + timedelta(hours=2) and timeslotuser.timeslot.starts > timezone.now():
+                print("Send reminder to " + timeslotuser.user.username + " for task " + str(timeslotuser.timeslot) + " (2 hours left)")
+                success = send_message_to_user(timeslotuser.user, "🔔 Hei, oppgaven `" + str(timeslotuser.timeslot) + "` starter om under to timer. Oppmøte er ved trappen utenfor logistikk.")
+                if success:
+                    timeslotuser.reminder_status = "ON"
+                    timeslotuser.save()
+    except Exception as e:
+        print(e)
 
 timeloop.start()
